45ced16489d93cb7bdf53e11b8e9c69baf317a0a
levinfritz <156805047+levinfritz@users.noreply.github.com>
2024-12-11 09:16:08 +0100

    konfig

--- Konfig2/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "web_server_ip" {
-  value = aws_instance.web_server.public_ip
-}
-
-output "db_server_ip" {
-  value = aws_instance.db_server.public_ip
-}
--- Konfig2/variables.tf
+++
@@ -1,4 +0,0 @@
-variable "region" {
-  description = "AWS Region"
-  default     = "us-east-1"
-}
