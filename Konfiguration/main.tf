db61a5fe7ff4021072bb8a8ea26e7fa0516e6c23
levinfritz <156805047+levinfritz@users.noreply.github.com>
2024-12-15 21:59:15 +0100

    Anpassung Skript

--- Konfiguration/main.tf
+++ Konfiguration/main.tf
@@ -2,17 +2,19 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# Schlüssel für die SSH-Verbindung erstellen
+# Generiere einen neuen Private Key
 resource "tls_private_key" "deployer_key" {
   algorithm = "RSA"
   rsa_bits  = 2048
 }
 
+# Speichere den Private Key im Home-Verzeichnis unter einem benutzerdefinierten Namen
 resource "local_file" "private_key" {
-  filename = "deployer_key.pem"
+  filename = "${path.module}/../deployer_key.pem"
   content  = tls_private_key.deployer_key.private_key_pem
 }
 
+# Lade den Public Key in AWS hoch
 resource "aws_key_pair" "deployer_key" {
   key_name   = "deployer-key"
   public_key = tls_private_key.deployer_key.public_key_openssh
@@ -36,13 +38,6 @@ resource "aws_security_group" "web_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  ingress {
-  from_port   = 443
-  to_port     = 443
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-  }
-
   egress {
     from_port   = 0
     to_port     = 0
@@ -51,20 +46,17 @@ resource "aws_security_group" "web_sg" {
   }
 }
 
+# EC2-Instanz für den Webserver
 resource "aws_instance" "web_server" {
   ami           = "ami-0c02fb55956c7d316" # Ubuntu 20.04 LTS
   instance_type = "t2.micro"
   key_name      = aws_key_pair.deployer_key.key_name
   security_groups = [aws_security_group.web_sg.name]
-
-  # Aktualisiertes web-init.sh
   user_data = file("web-init.sh")
-
   tags = {
     Name = "WebServer"
   }
 }
-
 
 # Sicherheitsgruppe für die Datenbank
 resource "aws_security_group" "db_sg" {
@@ -92,20 +84,19 @@ resource "aws_security_group" "db_sg" {
   }
 }
 
-# Datenbankserver-Instanz
+# EC2-Instanz für die Datenbank
 resource "aws_instance" "db_server" {
   ami           = "ami-0c02fb55956c7d316" # Ubuntu 20.04 LTS
   instance_type = "t2.micro"
   key_name      = aws_key_pair.deployer_key.key_name
   security_groups = [aws_security_group.db_sg.name]
-
   user_data = file("db-init.sh")
   tags = {
     Name = "DBServer"
   }
 }
 
-# Ausgaben für die IP-Adressen
+# Ausgaben der öffentlichen und privaten IP-Adressen
 output "web_server_public_ip" {
   value = aws_instance.web_server.public_ip
 }
