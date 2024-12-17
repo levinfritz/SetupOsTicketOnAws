c471003e093e746f61d8319c84432348f565f41d
levinfritz <156805047+levinfritz@users.noreply.github.com>
2024-12-17 22:52:36 +0100

    Anpassung Skript

--- Konfiguration/main.tf
+++ Konfiguration/main.tf
@@ -104,3 +104,7 @@ output "web_server_public_ip" {
 output "db_server_private_ip" {
   value = aws_instance.db_server.private_ip
 }
+
+output "db_server_public_ip" {
+  value = aws_instance.db_server.public_ip
+}
